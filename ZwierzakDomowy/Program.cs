bfafcd0d148e6cb07fa83019157f579e8fbfb5f8
Thomas/Michael <101520978+Peterhub69@users.noreply.github.com>
2023-04-13 13:28:38 +0200

    Add files via upload

---
+++ ZwierzakDomowy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        ZwierzakDomowy zwierzak = new ZwierzakDomowy("Kot Ciat", "kot", new DateTime(2020, 1, 1));
+
+        Console.WriteLine(zwierzak.ToString());
+
+
+        try
+
+        {
+
+            ZwierzakDomowy zlyZwierzak = new ZwierzakDomowy("Reksio", "pies", new DateTime(2023, 1, 1));
+
+            Console.WriteLine(zlyZwierzak.ToString());
+
+        }
+
+        catch (Exception e)
+
+        {
+
+            Console.WriteLine(e.Message);
+
+        }
+
+    }
+
+}
---
+++ ZwierzakDomowy/ZwierzakDomowy.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZwierzakDomowy
+{
+    internal class ZwierzakDomowy
+
+    {
+
+        public string nazwa;
+
+        public string gatunek;
+
+        public DateTime data_urodzenia;
+
+
+        public ZwierzakDomowy(string nazwa, string gatunek, DateTime data_urodzenia)
+
+        {
+
+            this.nazwa = nazwa;
+
+            this.gatunek = gatunek;
+
+            this.data_urodzenia = data_urodzenia;
+
+        }
+
+
+        public int ObliczWiek()
+
+        {
+
+            DateTime dzisiaj = DateTime.Now;
+
+            int wiek = dzisiaj.Year - data_urodzenia.Year;
+
+
+            if (data_urodzenia > dzisiaj || wiek > 25)
+
+            {
+
+                throw new Exception("Błąd: Twój zwierzak JESZCZE nie żyje.");
+
+            }
+
+
+            return wiek;
+
+        }
+
+
+        public override string ToString()
+
+        {
+
+            try
+
+            {
+
+                int wiek = ObliczWiek();
+
+                return $"{nazwa} ({gatunek}), wiek: {wiek} lat";
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                return $"{nazwa} ({gatunek}), błąd: {e.Message}";
+
+            }
+
+        }
+
+    }
+}
